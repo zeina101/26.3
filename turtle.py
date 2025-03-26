062b27f501d8142225536b55514c916c00b44194
zeina101 <zeina.syedain@cas.edu.pk>
2025-03-26 20:15:00 +0500

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+# creating canvas
+turtle.Screen().bgcolor("Orange")
+
+sc = turtle.Screen()
+sc.setup(400, 300)
+
+turtle.title("Welcome to Turtle Window.")
+
+# creating a square
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i = i+1
+
+    
